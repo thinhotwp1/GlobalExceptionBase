53d7d7b00655fd1e21f939b9037ac481ac8acd80
ThinhLD <leduythinh20001@gmail.com>
2023-09-04 12:54:27 +0700

    init

---
+++ GlobalExceptionBase/src/main/java/com/example/globalexceptionbase/GlobalExceptionBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalexceptionbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalExceptionBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalExceptionBaseApplication.class, args);
+    }
+
+}
---
+++ GlobalExceptionBase/src/test/java/com/example/globalexceptionbase/GlobalExceptionBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalexceptionbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalExceptionBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
